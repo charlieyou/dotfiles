85b77dee3652904844316288c93d582d8b787286
Charlie You <charlie.you@hey.com>
2026-01-12 22:01:22 +0000

    Add nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,102 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Enable true colors
+vim.opt.termguicolors = true
+
+-- Hybrid clipboard: OSC 52 for copy (works over SSH), xclip for paste (no blocking)
+vim.g.clipboard = {
+  name = 'OSC 52 + xclip',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = function() return vim.fn.systemlist('xclip -selection clipboard -o') end,
+    ['*'] = function() return vim.fn.systemlist('xclip -selection primary -o') end,
+  },
+}
+
+-- Plugins
+require("lazy").setup({
+  { "christoomey/vim-tmux-navigator" },
+  { "bullets-vim/bullets.vim" },
+
+  -- Colorscheme
+  {
+    "projekt0n/github-nvim-theme",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("github_dark_default")
+    end,
+  },
+
+  -- Treesitter for syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
+})
+
+-- LSP (built-in for Neovim 0.11+)
+vim.lsp.enable("gleam")
+
+-- Enable treesitter highlighting for all supported languages
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
+})
+
+-- Settings (defer to ensure plugins don't override)
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.opt.relativenumber = true
+    vim.opt.number = true
+  end,
+})
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.mouse = "a"
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.undofile = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.scrolloff = 8
+
+-- Per-language indent conventions
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "lua", "javascript", "typescript", "typescriptreact", "javascriptreact", "json", "yaml", "html", "css", "svelte", "vue", "gleam" },
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "go", "make" },
+  callback = function()
+    vim.bo.expandtab = false
+    vim.bo.shiftwidth = 4
+    vim.bo.tabstop = 4
+  end,
+})
+
+-- Keymaps
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("i", "kj", "<Esc>")
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
+vim.keymap.set("n", "B", "^")
+vim.keymap.set("n", "E", "$")
